4ea4a1be30cb2558694587a01ed09378902ac6cb
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-10-03 14:45:38 +0100

    update

--- math.js
+++ math.js
@@ -1,7 +1,9 @@
 const math = require("./server");
 
-const { subtract, multiply, divide } = require("./server");
-console.log(math.add(2, 3));
-console.log(math.subtract(2, 3));
-console.log(math.multiply(2, 3));
-console.log(math.divide(2, 3));
+
+
+//destructuring functions for the module
+exports.add = (a, b) => a + b;
+exports.subtract = (a, b) => a - b;
+exports.multiply = (a, b) => a * b;
+exports.divide = (a, b) => a / b;
--- server.js
+++ server.js
@@ -2,9 +2,11 @@ console.log("hello word");
 console.log(global)
 
 
-const { log } = require("console");
-const os = require("os");
-const path = require("path");
+// const { log } = require("console");
+// const os = require("os");
+// const path = require("path");
+
+
 
 // const add = (a, b) => a + b;
 // const subtract = (a, b) => a - b;
@@ -14,28 +16,30 @@ const path = require("path");
 // module.exports = {add,subtract,multiply,divide}
 
 
-
-export add = (a, b) => a + b;
-export subtract = (a, b) => a - b;
-export multiply = (a, b) => a * b;
-export divide = (a, b) => a / b;
-
-module.exports = {add,subtract,multiply,divide}
+const { add, subtract, multiply, divide} = require("./math");
 
 
+console.log(add(2, 3));
+console.log(subtract(2, 3));
+console.log(multiply(2, 3));
+console.log(divide(2, 3));
+
+// module.exports = {add,subtract,multiply,divide}
 
 
 
 
-const { dirname } = require("path");
-    console.log(os.type())           //Windows_NT
-    console.log(os.version());
-    console.log(os.homedir());
 
-    console.log(__dirname);
-    console.log(__filename);
-    console.log(path.dirname(__filename));
-    console.log(path.basename(__filename));
-    console.log(path.extname(__filename));
-    console.log(path.extname(__filename));
-    console.log(path.parse(__filename));
+
+// const { dirname } = require("path");
+//     console.log(os.type())           //Windows_NT
+//     console.log(os.version());
+//     console.log(os.homedir());
+
+//     console.log(__dirname);
+//     console.log(__filename);
+//     console.log(path.dirname(__filename));
+//     console.log(path.basename(__filename));
+//     console.log(path.extname(__filename));
+//     console.log(path.extname(__filename));
+//     console.log(path.parse(__filename));
